86db335455a4a7abb22bc6644804bfbcac80cd75
Phuc Tran <phuc978@gmail.com>
2016-02-04 20:31:26 -0500

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char** argv[])
+{
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Window Title");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+}
